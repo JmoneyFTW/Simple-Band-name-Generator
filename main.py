aa33ac631a85beea7975c8fbd3d076d899c62c03
JmoneyFTW <137325513+JmoneyFTW@users.noreply.github.com>
2024-01-18 06:44:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Shows the use of variables, use of length for strings, and input for user inputs.
-
 print("Welcome to the band name generator")
 city = input("What city are you from?\n")
 color = input("What is your favorite color?\n")
